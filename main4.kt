2de106e7ee8f3831a2ee265c5f05e15a0a9967f1
lseongmi <w2307@e-mirim.hs.kr>
2024-09-05 19:34:45 +0900

    빼기와 곱 function 추가

--- main4.kt
+++ main4.kt
@@ -4,11 +4,28 @@ fun main() {
     println(MyVar)//지역변수를 따른다
 
     var sum : Int = addFunction(10, 20)
+    var cha : Int = charFunction(20,5)
+    var gob: Int = gobFunction(10,20)
+
     println(sum)
+    println(cha)
+    println(gob)
 }
 
 fun addFunction(num1:Int, num2:Int):Int {
     var hap : Int
     hap = num1 + num2 + MyVar
     return hap
+}
+
+fun charFunction(num1:Int, num2:Int):Int {
+    var result : Int
+    result = num1 - num2
+    return result
+}
+
+fun gobFunction(num1:Int, num2:Int) : Int {
+    var result : Int
+    result = num1 * num2
+    return result
 }
