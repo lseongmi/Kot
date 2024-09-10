7ce1ec5ae537be3e96fe66e4618deab7a063f0eb
lseongmi <w2307@e-mirim.hs.kr>
2024-09-10 19:30:33 +0900

    빼기 추가

--- main6.kt
+++ main6.kt
@@ -2,4 +2,8 @@ fun main() {
     val mul = {x:Int, y:Int -> x*y}
     println(mul(2,3))
     println(mul(4,5))
+
+    val cha = {x:Int, y:Int -> x-y}
+    println(cha(9,8))
+    println(cha(5,3))
 }
